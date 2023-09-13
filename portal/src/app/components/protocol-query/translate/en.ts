3ee29b17c749d37fd3adfc827addcc927cad292b
iadibar <adi.bar1@intl.att.com>
2023-09-13 15:13:38 +0300

    fix style and add translate file - PROTOCOL_QUERY

---
+++ portal/src/app/components/protocol-query/translate/en.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/typedef
+export const PROTOCOL_QUERY_EN = {
+    ACTION_BUTTONS: {
+        RUN: 'Run',
+    },
+    FIELDS_LABEL: {
+        ITERATIONS_NUMBER: 'Number of iterations',
+        ALGORITHM: 'Algorithm(s)',
+    },
+    ALGORITHM_LABEL_DESCRIPTION: '1-3 algorithm(s) can be tested',
+};
+  
