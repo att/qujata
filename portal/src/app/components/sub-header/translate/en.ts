8f2fee0e0e6e1c9e95012d0684c334943c156fef
iadibar <adi.bar1@intl.att.com>
2023-10-11 09:01:41 +0300

    create new component for sub header

---
+++ portal/src/app/components/sub-header/translate/en.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/typedef
+export const SUB_HEADER_EN = {
+    TITLE: 'That’s what are we doing on each iteration:',
+    DESCRIPTIONS: {
+        STEP_1: 'Initiating handshake\nusing the selected\ncryptographic algorithm.',
+        STEP_2: 'Exchanging keys using\nthe selected algorithm to\ncreate a shared secret.',
+        STEP_3: 'Deriving symmetric keys,\nestablishing a secure channel\nfor communication.',
+        STEP_4: 'Downloading content\nover the secure channel.',
+        STEP_5: 'Closing\nthe session',
+    },
+    ALT_TITLES: {
+        HANDSHAKE: 'Handshake',
+        KEYS: 'Keys',
+        LOCK: 'Lock',
+        DOWNLOAD: 'Download',
+        CHECK: 'Check',
+    },
+};
+  
