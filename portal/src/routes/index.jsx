423c7840540bd897f18119a7e07bf6babe79d650
Ohad Koren <ohad.koren@intl.att.com>
2023-12-12 12:43:46 +0200

    /qujata route for integration

--- portal/src/routes/index.jsx
+++ portal/src/routes/index.jsx
@@ -9,7 +9,7 @@ export const router = createBrowserRouter([
         element: <Root />,
         children: [
           {
-            path: '/',
+            path: '/qujata',
             element: <Home />,
             children: [
                 {
