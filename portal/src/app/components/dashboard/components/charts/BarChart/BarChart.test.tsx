391e134a7af68bc0c1e7124d4e21c0d5f3bde037
Ohad Koren <ohad.koren@intl.att.com>
2023-12-17 16:10:11 +0200

    Refactor tooltip keys and labels in BarChart

--- portal/src/app/components/dashboard/components/charts/BarChart/BarChart.test.tsx
+++ portal/src/app/components/dashboard/components/charts/BarChart/BarChart.test.tsx
@@ -1,78 +1,52 @@
 import { render, RenderResult } from '@testing-library/react';
 import { BarChart, generateTooltipTitle, renderTooltipLabel } from './BarChart';
-import Chart from 'chart.js';
 
 jest.mock('react-chartjs-2', () => ({
-    Bar: () => <div data-testid="chart">Chart</div>, // add data-testid attribute
+    Bar: () => <div data-testid="chart">Chart</div>,
 }));
 jest.mock('chart.js');
 
-const tooltipKeys = ['messageSizeBytes', 'iterations'];
-const tooltipLabels = ['Message Size', 'Iterations'];
+const tooltipKeys = ['algorithm', 'iterations'];
+const tooltipLabels = ['Algorithm', 'Iterations'];
 const data = [
     {
         "algorithm": "Algorithm1",
         "iterations": 1,
-        "messageSizeBytes": 12,
         "results": {
             "averageCPU": 5,
             "averageMemory": 512,
-            "errorRate": 0.05,
-            "bytesThroughput": 2048000,
-            "messagesThroughput": 500,
-            "averageTLSHandshakeTime": 10.2
         }
     },
     {
         "algorithm": "Algorithm1",
         "iterations": 133,
-        "messageSizeBytes": 1233,
         "results": {
             "averageCPU": 533,
             "averageMemory": 512,
-            "errorRate": 0.05,
-            "bytesThroughput": 2048000,
-            "messagesThroughput": 500,
-            "averageTLSHandshakeTime": 10.2
         }
     },
     {
         "algorithm": "Algorithm1",
         "iterations": 1000,
-        "messageSizeBytes": 102,
         "results": {
             "averageCPU": 25,
             "averageMemory": 512,
-            "errorRate": 0.05,
-            "bytesThroughput": 2048000,
-            "messagesThroughput": 500,
-            "averageTLSHandshakeTime": 10.2
         }
     },
     {
         "algorithm": "Algorithm2",
         "iterations": 2000,
-        "messageSizeBytes": 124,
         "results": {
             "averageCPU": 28,
             "averageMemory": 512,
-            "errorRate": 0.05,
-            "bytesThroughput": 2048000,
-            "messagesThroughput": 500,
-            "averageTLSHandshakeTime": 10.2
         }
     },
     {
         "algorithm": "Algorithm3",
         "iterations": 2000,
-        "messageSizeBytes": 124,
         "results": {
             "averageCPU": 28,
             "averageMemory": 512,
-            "errorRate": 0.05,
-            "bytesThroughput": 2048000,
-            "messagesThroughput": 500,
-            "averageTLSHandshakeTime": 10.2
         }
     }
 ];
@@ -86,7 +60,7 @@ describe('BarChart', () => {
   test('should return correct label', () => {
     const tooltipItem = { datasetIndex: 0, index: 0 };
     const result = renderTooltipLabel(tooltipItem, [5, 533, 25, 28, 28], tooltipKeys, tooltipLabels, data);
-    expect(result).toBe('5 (Message Size: 12, Iterations: 1)');
+    expect(result).toBe('5 (Algorithm: Algorithm1, Iterations: 1)');
   });
 
   test('should return correct title', () => {
--- portal/src/app/components/home/components/experiment/components/charts/models/bar-chart.const.ts
+++ portal/src/app/components/home/components/experiment/components/charts/models/bar-chart.const.ts
@@ -1,23 +1,15 @@
-import { CHARTS_EN } from "../translate/en";
+import { CHARTS_EN } from '../translate/en';
 
-export const tooltipKeys = ['messageSizeBytes', 'iterations'];
-export const tooltipLabels = ['Message Size', 'Iterations'];
+export const tooltipKeys = ['algorithm', 'iterations'];
+export const tooltipLabels = ['Algorithm', 'Iterations'];
 
 export enum ChartKey {
     averageCPU = 'averageCPU',
     averageMemory = 'averageMemory',
-    errorRate = 'errorRate',
-    bytesThroughput = 'bytesThroughput',
-    messagesThroughput = 'messagesThroughput',
-    averageTLSHandshakeTime = 'averageTLSHandshakeTime',
 }
 
 export type ChartTitleDisplayMappingType = { [key in keyof typeof ChartKey]: string; }
 export const ChartTitleDisplayMapping: ChartTitleDisplayMappingType = {
   [ChartKey.averageCPU]: CHARTS_EN.CHART_TITLES.AVERAGE_CPU,
   [ChartKey.averageMemory]: CHARTS_EN.CHART_TITLES.AVERAGE_MEMORY,
-  [ChartKey.errorRate]: CHARTS_EN.CHART_TITLES.ERROR_RATE,
-  [ChartKey.bytesThroughput]: CHARTS_EN.CHART_TITLES.BYTES_THROUGHPUT,
-  [ChartKey.messagesThroughput]: CHARTS_EN.CHART_TITLES.MESSAGES_THROUGHPUT,
-  [ChartKey.averageTLSHandshakeTime]: CHARTS_EN.CHART_TITLES.AVERAGE_TLS_HANDSHAKE_TIME,
 } as unknown as ChartTitleDisplayMappingType;
