056eb8220e3371edc0c5ab2d82f10dc427f0efc2
iadibar <adi.bar1@intl.att.com>
2024-01-03 10:07:04 +0200

    fix key error

--- portal/src/app/components/home/components/experiment/components/charts/utils/chart.utils.ts
+++ portal/src/app/components/home/components/experiment/components/charts/utils/chart.utils.ts
@@ -2,7 +2,7 @@ import { IResult, ITestRunResultData } from '../../../../../../../shared/models/
 import { ChartKey, ChartTitleDisplayMapping } from '../models/bar-chart.const';
 
 export function getLabels(data: ITestRunResultData[]): string[] {
-    return data.map((obj: ITestRunResultData, index: number) => `${obj.algorithm}       `);
+    return data.map((obj: ITestRunResultData, index: number) => `${obj.algorithm} (iteration ${index + 1})       `);
 }
 
 export function getKeysOfData(results: IResult): string[] {
