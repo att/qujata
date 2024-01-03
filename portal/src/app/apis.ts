36491318f38310e23b9b0822146591a3c60326e1
Ohad Koren <ohad.koren@intl.att.com>
2024-01-03 12:40:53 +0200

    Refactor API URLs and update table options styling

--- portal/src/app/apis.ts
+++ portal/src/app/apis.ts
@@ -1,13 +1,12 @@
-const prefix = 'qujata-api';
 const testSuites = 'test_suites';
 
 export const APIS: { [key in keyof typeof API_URLS]: string } = {
   analyze: 'analyze',
   algorithms: 'algorithms',
   iterations: 'iterations',
-  testRunResults: `${prefix}/${testSuites}/:testSuiteId`,
-  editExperiment: `${prefix}/${testSuites}/:testSuiteId`,
-  deleteExperiment: `${prefix}/${testSuites}/:testSuiteId`,
+  testRunResults: `${testSuites}/:testSuiteId`,
+  editExperiment: `${testSuites}/:testSuiteId`,
+  deleteExperiment: `${testSuites}/:testSuiteId`,
 };
 
 enum API_URLS {
--- portal/src/app/components/home/components/experiment/components/table-options/TableOptions.tsx
+++ portal/src/app/components/home/components/experiment/components/table-options/TableOptions.tsx
@@ -1,7 +1,10 @@
 import styles from './TableOptions.module.scss';
+import cn from 'classnames';
 import { TABLE_OPTIONS_EN } from './translate/en';
 import SelectColumnsSvg from '../../../../../../../assets/images/select-columns.svg';
+import SelectColumnsCheckedSvg from '../../../../../../../assets/images/select-columns-checked.svg';
 import EyeSvg from '../../../../../../../assets/images/eye.svg';
+import EyeSvgHover from '../../../../../../../assets/images/eye-hover.svg';
 import { Button, ButtonActionType, ButtonSize, ButtonStyleType } from '../../../../../../shared/components/att-button';
 import { ExternalLink, LinkRel, LinkSize, LinkStyle, LinkTarget } from '../../../../../../shared/components/att-link';
 import { useEffect, useState } from 'react';
@@ -11,6 +14,7 @@ import { ITestRunResult } from '../../../../../../shared/models/test-run-result.
 import { initialLink } from './constants/table-options.const';
 
 const SelectColumnsAriaLabel: string = TABLE_OPTIONS_EN.SELECT_COLUMNS;
+const ViewInGrafanaAriaLabel: string = TABLE_OPTIONS_EN.VIEW_TO_GRAFANA;
 
 export interface TableOptionsProps {
   data: ITestRunResult;
@@ -38,9 +42,10 @@ export const TableOptions: React.FC<TableOptionsProps> = (props: TableOptionsPro
         target={LinkTarget.BLANK}
         rel={LinkRel.NO_OPENER}
       >
-        <div className={styles.grafana_link}>
-            <img className={styles.eye_icon} src={EyeSvg} alt="eye" />
-            {TABLE_OPTIONS_EN.VIEW_TO_GRAFANA}
+        <div className={styles.view_in_grafana_wrapper}>
+            <img className={cn(styles.eye_icon, styles.default_image)} src={EyeSvg} alt={ViewInGrafanaAriaLabel} />
+            <img className={cn(styles.eye_icon, styles.hover_image)} src={EyeSvgHover} alt={ViewInGrafanaAriaLabel} />
+            <span className={styles.options_text}>{TABLE_OPTIONS_EN.VIEW_TO_GRAFANA}</span>
         </div>
       </ExternalLink>
       <Button
@@ -51,8 +56,9 @@ export const TableOptions: React.FC<TableOptionsProps> = (props: TableOptionsPro
         onButtonClick={props.handleSelectColumnsClick}
       >
         <div className={styles.options_wrapper}>
-          <img className ={styles.select_columns_icon} src={SelectColumnsSvg} alt={SelectColumnsAriaLabel} />
-          <span className={styles.select_columns_text}>{TABLE_OPTIONS_EN.SELECT_COLUMNS}</span>
+          <img className ={cn(styles.select_columns_icon, styles.default_image)} src={SelectColumnsSvg} alt={SelectColumnsAriaLabel} />
+          <img className ={cn(styles.select_columns_icon, styles.hover_image)} src={SelectColumnsCheckedSvg} alt={SelectColumnsAriaLabel} />
+          <span className={styles.options_text}>{TABLE_OPTIONS_EN.SELECT_COLUMNS}</span>
         </div>
       </Button>
     </div>
